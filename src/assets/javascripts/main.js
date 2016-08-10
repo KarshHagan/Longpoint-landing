d602df1c5e4c614e77083e5734ce0c4caa597e40
josephsap <joesap1@gmail.com>
2016-08-10 16:12:09 -0600

    nav highlight on scroll to in

--- src/assets/javascripts/main.js
+++ src/assets/javascripts/main.js
@@ -2,25 +2,73 @@ var Main = {
 
   init: function() {
     var _this = this;
-    this.navScrollTo();
+
+    this.$categoryMenuItems = $('.scroll-link');
+    this.$categoryMenuHeight = $('nav').outerHeight() + 13;
+    this.$scrollItems = this.$categoryMenuItems.map(function() {
+      var item = $($(this).attr('href'));
+      if(item.length) {
+        return item;
+      }
+    });
+
+    this.$categoryMenuItems.on('click', function(e) {
+      _this.navScrollTo(e);
+    });
+
     this.desktopImages();
+    this.navActiveOnScroll();
+    this.handleWindowScroll();
     $(window).on('resize', function() {
       _this.handleWindowResize();
     });
+
+    this.wasNavItemClicked = false;
   },
 
-  navScrollTo: function() {
-    $('.scroll-link').on('click', function(e) {
-      e.preventDefault();
+  navScrollTo: function(e) {
+    $this = $(e.currentTarget),
+    target = $this.attr('href'),
+    gotoTarget = $(target);
+    e.preventDefault();
+    var _this = this;
+    this.wasNavItemClicked = true;
+    $('.scroll-link').removeClass('active');
+    $(this).addClass('active');
+    $('body, html').animate({
+      scrollTop: gotoTarget.offset().top.toFixed(0) - 2
+    }, 250, function() {
+      _this.wasNavItemClicked = false;
+    });
 
-      var _this = $(this);
-      $('.scroll-link').parent().removeClass('active');
-      window.location.hash = $(this).attr('href').replace('#', '_');
-      $('body, html').animate({
-        scrollTop: $($(this).attr('href')).offset().top - 10
-      }, 250, function() {
-        _this.parent().addClass('active');
-      });
+  },
+
+  navActiveOnScroll: function() {
+    var currentScrollTop = $(document).scrollTop();
+    var _fromTop = currentScrollTop + this.$categoryMenuHeight;
+    var _currentItem = this.$scrollItems.map(function() {
+      if($(this).offset().top < _fromTop) {
+        return this;
+      }
+    });
+    _currentItem = _currentItem[_currentItem.length - 1];
+
+    var _id = _currentItem && _currentItem.length ? _currentItem[0].id : " ";
+
+    this.$categoryMenuItems.removeClass("active").filter("[href=#"+_id+"]").addClass("active");
+    
+    if(currentScrollTop <= 100) {
+      $('[href="#intro"]').addClass('active');
+    }
+  },
+
+  handleWindowScroll: function() {
+    var _this = this;
+    this.wasNavItemClicked = false;
+    $(window).on('scroll', function() {
+      if(_this.wasNavItemClicked == false) {
+        _this.navActiveOnScroll();
+      }
     });
   },
 
